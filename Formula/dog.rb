5e5a111151047169ce62c903ed3cfe8bc2cb5c22
griffin <griffin@gmail.com>
2025-06-12 01:14:16 +0900

    修复：将中文替换为ASCII字符以解决编码问题

--- Formula/dog.rb
+++ Formula/dog.rb
@@ -1,5 +1,5 @@
 class Dog < Formula
-  desc "私人 dog 工具"
+  desc "Personal dog tool"
   homepage "https://github.com/griffinsin/dog"
   version "1.0.8"
   license "MIT"
@@ -7,24 +7,24 @@ class Dog < Formula
   url "https://github.com/griffinsin/dog/archive/refs/tags/v1.0.8.tar.gz"
   sha256 "1c8afc5545fd01e00b6ab3843678dcc7143b368965ab4efe332c4504c4a1a8d2"
   
-  # 注意：sha256值需要在创建GitHub release后更新为实际值
+  # Note: sha256 value will be updated automatically after GitHub release is created
 
   def install
     bin.install "bin/dog"
     prefix.install "lib"
     prefix.install "bin/commands"
     
-    # 修改dog脚本中的路径引用
+    # Update path references in dog script
     inreplace bin/"dog", "$(dirname \"$(dirname \"${BASH_SOURCE[0]}\")\")/lib/globals.sh", "#{prefix}/lib/globals.sh"
     
-    # 修改命令路径引用 - 修复检查和执行命令的部分
+    # Fix command path references for command checking
     inreplace bin/"dog", "if [[ ! -f \"${COMMANDS_DIR}/$1.sh\" ]]; then", "if [[ ! -f \"#{prefix}/commands/$1.sh\" ]]; then"
     
-    # 修复命令执行部分，保存命令名称后再shift
-    inreplace bin/"dog", "# 执行命令\nshift\nsource \"${COMMANDS_DIR}/$1.sh\" \"$@\"", "# 执行命令\nCMD=$1\nshift\nsource \"#{prefix}/commands/$CMD.sh\" \"$@\""
+    # Fix command execution by saving command name before shift
+    inreplace bin/"dog", "# 执行命令\nshift\nsource \"${COMMANDS_DIR}/$1.sh\" \"$@\"", "# Execute command\nCMD=$1\nshift\nsource \"#{prefix}/commands/$CMD.sh\" \"$@\""
   end
 
   test do
-    assert_match "版本 #{version}", shell_output("#{bin}/dog -v")
+    assert_match "version #{version}", shell_output("#{bin}/dog -v")
   end
 end
